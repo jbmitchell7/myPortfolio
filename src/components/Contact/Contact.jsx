71441d5179f328098079138cb8f7d6068c3cab23
jbmitchell7 <jbmitchell747@gmail.com>
2022-01-27 15:29:40 -0600

    finished contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Contact.scss';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const Contact = () => (
   <div id="contact" className='section'>
-    <h1 className='section-header'>Contact Me</h1>
+    <h1 className='section-header'>Get in Touch with Me</h1>
+    <div className='card-container'>
+      <Card className='contact-card'>
+        <Card.Body className='contact-card-body'>
+          <h3 className='card-title'>
+            If you'd like to talk to me about
+            employment opportunities, my Github projects,
+            or anything in between; contact me in your preferred
+            method below!
+          </h3>
+          <Button className='contact-btn' href="mailto:jbmitchell747@gmail.com">Email Me!</Button>
+          <Button className='contact-btn' href="https://github.com/jbmitchell7" target="_blank">My Github Profile</Button>
+          <Button className='contact-btn' href="https://linkedin.com" target="_blank">Connect with Me on LinkedIn</Button>
+        </Card.Body>
+      </Card>
+    </div>
   </div>
 );
 
