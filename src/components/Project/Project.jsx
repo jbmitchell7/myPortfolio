744d14951a8d8e884f383313f3dbf08df9062cdf
jbmitchell7 <jbmitchell747@gmail.com>
2022-01-27 16:24:38 -0600

    working on project modals

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,23 +1,38 @@
 import './Project.scss';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
 
 const Project = (props) => {
   const { project } = props;
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Card className='project-card'>
-      <Card.Body className='project-card-body'>
-        <Card.Title>{project.title}</Card.Title>
-        <Card.Img src={project.screenshot} alt="" className='project-screenshot' />
-        <Button className='project-button' href={project.github} target="_blank">Github Source</Button>
-        <Button className='project-button' href={project.projectURL} target="_blank">See Project</Button>
-      </Card.Body>
-    </Card>
+    <>
+      <Button onClick={handleShow} className="open-modal-btn">
+        <Card className='project-card'>
+          <Card.Body className='project-card-body'>
+            <h4 className='project-title'>{project.title}</h4>
+            <Card.Img src={project.screenshot} alt="" className='project-screenshot' />
+          </Card.Body>
+        </Card>
+      </Button>
+      <Modal show={show} onHide={handleClose} className='project-modal'>
+        <Modal.Header closeButton>
+          <Modal.Title>{project.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={project.screenshot} alt="project-screenshot" />
+          <Button className='project-button' href={project.github} target="_blank">Github Source</Button>
+          <Button className='project-button' href={project.projectURL} target="_blank">See Project</Button>
+        </Modal.Body>
+      </Modal>
+    </>
   )
 };
-
-Project.propTypes = {};
-
-Project.defaultProps = {};
 
 export default Project;
--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -7,6 +7,7 @@ import Project from '../Project/Project';
 import './ProjectsList.scss';
 
 const ProjectsList = () => {
+
   return (
     <div className="section" id="projects" >
       <h1 className='section-header'>Projects</h1>
