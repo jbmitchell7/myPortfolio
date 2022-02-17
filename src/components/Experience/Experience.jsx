88cd30b7ea90b9f10e8f8e02822958b4f7ef17c9
jbmitchell7 <jbmitchell747@gmail.com>
2022-02-17 08:33:12 -0600

    fixed logo error and experience height issue

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => (
     <h1 className='section-header'>Experience</h1>
     <Container className='experience-container'>
       <Row>
-        <Col xxl={6}>
+        <Col xxl={6} className='first-card'>
           <Card className="experience-card">
             <h3 className='experience-title'>Education</h3>
             {education.map(e => (
@@ -20,7 +20,7 @@ const Experience = () => (
             ))}
           </Card>
         </Col>
-        <Col xxl={6}>
+        <Col xxl={6} className='first-card'>
           <Card className="experience-card">
             <h3 className='experience-title'>Work</h3>
             {work.map(w => (
