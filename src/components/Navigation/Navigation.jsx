7ead94f6463f424a05894a9febea623bd14cf92d
jbmitchell7 <jbmitchell747@gmail.com>
2024-10-01 16:33:25 -0500

    convert to new render function

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,9 +5,6 @@ import './Navigation.scss';
 const Navigation = () => (
   <Navbar className="color-nav" collapseOnSelect expand="md" variant="dark" sticky="top">
     <Container>
-      {/* <Navbar.Brand href="#home">
-        <img src="img/logo.png" className='logo' alt="logo" />
-      </Navbar.Brand> */}
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="links">
@@ -15,7 +12,6 @@ const Navigation = () => (
           <Nav.Link href="#about" className="link-item">About</Nav.Link>
           <Nav.Link href="#experience" className="link-item">Experience</Nav.Link>
           <Nav.Link href="#projects" className="link-item">Projects</Nav.Link>
-          {/* <Nav.Link href="#caseStudy" className="link-item">Case Study</Nav.Link> */}
           <Nav.Link href="#contact" className="link-item">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
