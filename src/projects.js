8b2b17b559aee1f24625482e74fdce8d36604389
jbmitchell7 <jbmitchell747@gmail.com>
2024-10-01 16:14:32 -0500

    change portfolio url

--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,7 @@ export const projects = [
         "description": "This is an Angular website that uses the Sleeper App API to display standings and a bubble chart from ng2-charts to show you live info on your favorite Sleeper fantasy league. If you have a league ID enter it in the field, if not feel free to use 725424222041657344 as an example.",
         "screenshot": "img/projects/sff.png",
         "github": "https://github.com/jbmitchell7/sleeperfantasyfocus",
-        "projectURL": "https://www.jakemitchell.cloud/sleeperfantasyfocus"
+        "projectURL": "https://portfolio.thundercloud.dev/sleeperfantasyfocus"
     },
     {
         "title": "Flickbase",
@@ -20,46 +20,4 @@ export const projects = [
         "github": "https://github.com/jbmitchell7/Space_Renegade",
         "projectURL": "https://github.com/jbmitchell7/Space_Renegade"
     },
-    // {
-    //     "title": "Yakker",
-    //     "description": "Yakker is a React Native Chat Application. Users can send basics texts, photos from the camera or the gallery, and their location. They can also choose a background color for the chat. It integrates with Firebase to store past chat data.",
-    //     "screenshot": "img/projects/yakker.png",
-    //     "github": "https://github.com/jbmitchell7/yakker",
-    //     "projectURL": "https://github.com/jbmitchell7/yakker"
-    // },
-    // {
-    //     "title": "JakesMovieDB API",
-    //     "description": "This is a custom built REST API for serving movie data to my React and Angular clients. It is hosted using Heroku.",
-    //     "screenshot": "img/projects/rest.png",
-    //     "github": "https://github.com/jbmitchell7/myFlix",
-    //     "projectURL": "https://jakesmoviedb.herokuapp.com/documentation.html"
-    // },
-    // {
-    //     "title": "JakesMovieDB Angular",
-    //     "description": "This is a rewrite of the JakesMovieDB React client using Angular. It uses the same custom build REST API as the React version.",
-    //     "screenshot": "img/projects/myflix-angular.png",
-    //     "github": "https://github.com/jbmitchell7/myFlix-Angular-Client",
-    //     "projectURL": "https://www.jakemitchell.cloud/myFlix-Angular-Client"
-    // },
-    // {
-    //     "title": "Meetups",
-    //     "description": "Meetups is a React App that pulls calendar data from the Google Calendar API and displays the events to the user. It implements Lambda Functions in AWS to handle Google Authentication.",
-    //     "screenshot": "img/projects/meetups.png",
-    //     "github": "https://github.com/jbmitchell7/meetups",
-    //     "projectURL": "https://www.jakemitchell.cloud/meetups"
-    // },
-    // {
-    //     "title": "Pokedex",
-    //     "description": "This is a Javascript app that uses the PokeAPI to pull the name, types, stats, and sprite of a Pokemon. It includes a search function to filter by name, as well as a modal that gives more detail on a Pokemon.",
-    //     "screenshot": "img/projects/pokedex.png",
-    //     "github": "https://github.com/jbmitchell7/pokedex-jsproject",
-    //     "projectURL": "https://www.jakemitchell.cloud/pokedex-jsproject"
-    // },
-    // {
-    //     "title": "JakesMovieDB React",
-    //     "description": "This is a full-stack ReactJS application that accesses a custom built REST API and displays data on movies. It is built on the MERN stack and allows users to create an account and add and remove movies to their favorites.",
-    //     "screenshot": "img/projects/myflix.PNG",
-    //     "github": "https://github.com/jbmitchell7/myFlix-client",
-    //     "projectURL": "https://www.jakemitchell.cloud/myFlix-client"
-    // },
 ]
