d82c582dcb09814248545b1fa9d19ed692e72106
jbmitchell7 <coding+github@thundercloud.dev>
2025-02-27 03:13:18 +0000

    update project descriptions and source repos

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -3,20 +3,22 @@ export interface ProjectItem {
 	description: string;
 	screenshot: string;
 	github: string;
-	projectUrl: string;
+  githubSecondary?: string;
+	projectUrl?: string;
 }
 
 export const projects: ProjectItem[] = [
 	{
 		title: "Sleeper Fantasy Focus",
-		description: "This is an Angular website that uses the Sleeper App API to display standings and a bubble chart from ng2-charts to show you live info on your favorite Sleeper fantasy league. If you have a league ID enter it in the field, if not feel free to use 725424222041657344 as an example.",
+		description: "This is an MEAN stack project that uses the Sleeper App API to display live data and charts on your favorite Sleeper fantasy league. If you have a league ID enter it in the field, if not feel free to use 725424222041657344 as an example.",
 		screenshot: "img/projects/sff.png",
 		github: "https://github.com/jbmitchell7/sleeperfantasyfocus",
+    githubSecondary: "https://github.com/jbmitchell7/sleeperfantasyfocus-be",
 		projectUrl: "https://portfolio.thundercloud.dev/sleeperfantasyfocus"
 	},
 	{
 		title: "Flickbase",
-		description: "Flickbase is a front end for themoviedb.org API and is currently in early access on the Google Play Store. It includes the ability to log in to existing TMDB accounts, as well as creating and updating a watchlist unique to the flickbase app. Users can search for movies, tv shows, and people in the film and television industry.",
+		description: "Flickbase is REact Native project that is a front end for themoviedb.org API on the Google Play Store. It includes the ability to log in to existing TMDB accounts, as well as creating and updating a watchlist unique to the flickbase app. Users can search for movies, tv shows, and people in the film and television industry.",
 		screenshot: "img/projects/flickbase.png",
 		github: "https://github.com/jbmitchell7/flickbase",
 		projectUrl: "https://play.google.com/store/apps/details?id=cloud.jakemitchell.flickbase"
@@ -25,7 +27,6 @@ export const projects: ProjectItem[] = [
 		title: "Space Renegade",
 		description: "Space Renegade is a game built using Python and the Kivy engine. It is a pixel art game where you control a ship and must dodge incoming asteroids while trying to shoot down alien ships. The more alien ships you destroy, the higher your score. It can be run on a computer using Python.",
 		screenshot: "img/projects/space-renegade.png",
-		github: "https://github.com/jbmitchell7/Space_Renegade",
-		projectUrl: "https://github.com/jbmitchell7/Space_Renegade"
+		github: "https://github.com/jbmitchell7/Space_Renegade"
 	},
 ]
