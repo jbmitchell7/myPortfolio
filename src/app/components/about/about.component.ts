c6fc74e124b9d02f5f13b670c5cea04117dfc218
jbmitchell7 <coding+github@thundercloud.dev>
2025-02-27 00:12:05 +0000

    fixes resume button

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { skills } from '../../data/skills';
-import { ButtonModule } from 'primeng/button';
 import { SectionWrapperComponent } from '../section-wrapper/section-wrapper.component';
 
 @Component({
   selector: 'about',
-  imports: [ButtonModule, SectionWrapperComponent],
+  imports: [SectionWrapperComponent],
   templateUrl: './about.component.html'
 })
 export class AboutComponent {
