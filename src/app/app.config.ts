33e37e36c1c86a65681e54031feca53ce13edfe9
jbmitchell7 <coding+github@thundercloud.dev>
2025-02-27 02:46:47 +0000

    final cleanup and theme color change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import { myTheme } from './app.theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura,
+            preset: myTheme,
             options: {
               darkModeSelector: '.dark-mode'
             }
